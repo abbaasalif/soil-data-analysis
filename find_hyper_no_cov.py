3a52261a0159600765482ade8731a735dc2055b9
Ubuntu <ubuntu@ip-172-31-10-87.us-east-2.compute.internal>
2023-04-25 17:07:13 +0000

    first commit

---
+++ find_hyper_no_cov.py
@@ -0,0 +1,349 @@
+import os
+LOAD = False         # True = load previously saved model from disk?  False = (re)train the model
+SAVE = "\_TFT_model_02.pth.tar"   # file name to save the model under
+
+EPOCHS = 200
+INLEN = 16          # input size
+HIDDEN = 32         # hidden layers    
+LSTMLAYERS = 2      # recurrent layers
+
+ATTH = 2            # attention heads
+BATCH = 64          # batch size
+LEARN = 1e-3        # learning rate
+DROPOUT = 0.1       # dropout rate
+VALWAIT = 1         # epochs to wait before evaluating the loss on the test/validation set
+N_FC = 2            # output size
+
+RAND = 42           # random seed
+N_SAMPLES = 100     # number of times a prediction is sampled from a probabilistic model
+N_JOBS = -1          # parallel processors to use;  -1 = all processors
+
+# default quantiles for QuantileRegression
+QUANTILES = [0.01, 0.1, 0.2, 0.5, 0.8, 0.9, 0.99]
+
+SPLIT = 0.9         # train/test %
+
+FIGSIZE = (9, 6)
+
+
+qL1, qL2 = 0.01, 0.10        # percentiles of predictions: lower bounds
+qU1, qU2 = 1-qL1, 1-qL2,     # upper bounds derived from lower bounds
+label_q1 = f'{int(qU1 * 100)} / {int(qL1 * 100)} percentile band'
+label_q2 = f'{int(qU2 * 100)} / {int(qL2 * 100)} percentile band'
+
+mpath = os.path.abspath(os.getcwd()) + SAVE     # path and file name to save the model
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import sys
+import missingno as mno
+
+import warnings
+warnings.filterwarnings("ignore")
+import logging
+logging.disable(logging.CRITICAL)
+
+
+from darts import TimeSeries, concatenate
+from darts.dataprocessing.transformers import Scaler
+from darts.models import TransformerModel, NBEATSModel, TFTModel
+from darts.metrics import mape, rmse, smape
+from darts.utils.timeseries_generation import datetime_attribute_timeseries
+from darts.utils.likelihood_models import QuantileRegression
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+
+pd.set_option("display.precision",2)
+np.set_printoptions(precision=2, suppress=True)
+pd.options.display.float_format = '{:,.2f}'.format
+
+import pandas as pd
+xls = pd.ExcelFile('./data.xlsx')
+df_radon={}
+for key in ['1']:
+    df_radon[key] = pd.read_excel(xls, key)
+    
+#using 21 as the example since it has the most data
+df = df_radon['1']
+
+df = df[['SyncDate','Temperature','Humidity','AirPressure','Radon']]
+
+df['SyncDate'] = pd.to_datetime(df['SyncDate'])
+df.sort_values(by='SyncDate', ascending=True, inplace=True)
+df['SyncDate'] = df['SyncDate'].dt.floor('H')
+df = df.resample('H', on = 'SyncDate').mean()
+df = df.interpolate(method='linear', limit_direction='both')
+
+df = df[(df.index>='2022-12-22 19:00:00') & (df.index<='2023-02-08 18:00:00')]
+
+df3 = pd.DataFrame(index=df.index)
+df3['Radon'] = df['Radon']
+
+# create time series object for target variable
+ts_P = TimeSeries.from_series(df3["Radon"], fill_missing_dates=True, freq="H") 
+
+# check attributes of the time series
+print("components:", ts_P.components)
+print("duration:",ts_P.duration)
+print("frequency:",ts_P.freq)
+print("frequency:",ts_P.freq_str)
+print("has date time index? (or else, it must have an integer index):",ts_P.has_datetime_index)
+print("deterministic:",ts_P.is_deterministic)
+print("univariate:",ts_P.is_univariate)
+
+
+# train/test split and scaling of target variable
+ts_train, ts_test = ts_P.split_after(split_point=1104)
+print("training start:", ts_train.start_time())
+print("training end:", ts_train.end_time())
+print("training duration:",ts_train.duration)
+print("test start:", ts_test.start_time())
+print("test end:", ts_test.end_time())
+print("test duration:", ts_test.duration)
+
+
+scalerP = Scaler()
+scalerP.fit_transform(ts_train)
+ts_ttrain = scalerP.transform(ts_train)
+ts_ttest = scalerP.transform(ts_test)    
+ts_t = scalerP.transform(ts_P)
+
+# make sure data are of type float
+ts_t = ts_t.astype(np.float32)
+ts_ttrain = ts_ttrain.astype(np.float32)
+ts_ttest = ts_ttest.astype(np.float32)
+
+import torch
+from darts.utils.losses import SmapeLoss
+from torchmetrics import MetricCollection, SymmetricMeanAbsolutePercentageError, MeanAbsolutePercentageError
+def build_fit_tft_model(
+    model_args,
+    save_checkpoints=False,
+    callbacks=None,
+    save_model=False
+):
+    BATCH_SIZE=64
+    MAX_EPOCHS=500
+    NR_EPOCHS_VAL_PERIOD=1
+
+    torch_metrics = MetricCollection([MeanAbsolutePercentageError(), SymmetricMeanAbsolutePercentageError()])
+
+#     early_stopper = EarlyStopping(
+#         monitor="val_loss",
+#         patience=5,
+#         min_delta=0.001,
+#         mode='min',
+#     )
+
+#     if callbacks is None:
+#         callbacks = [early_stopper]
+#     else:
+#         callbacks.append(early_stopper)
+    
+    #detect if GPU is available
+#     if torch.cuda.is_available():
+#         pl_trainer_kwargs = {
+#             "accelerator": "gpu",
+#             "gpus": -1,
+#             "auto_select_gpus": True,
+#             "callbacks": callbacks,
+#             "enable_progress_bar":False,
+#         }
+#         num_workers=8
+#     else:
+#         pl_trainer_kwargs={
+#             "callbacks": callbacks,
+#         }
+#         num_workers=0
+    pl_trainer_kwargs={
+            "accelerator": "gpu",
+            "gpus":-1,
+            "auto_select_gpus": True,
+            "callbacks": callbacks,
+            "enable_progress_bar": False,
+        }
+    encoders={"cyclic": {"future": ["hour"]}} if model_args['include_hour'] else None
+
+
+    model = TFTModel(
+        input_chunk_length=model_args['in_len'],
+        output_chunk_length=model_args['out_len'],
+        batch_size=BATCH_SIZE,
+        n_epochs=MAX_EPOCHS,
+        nr_epochs_val_period=NR_EPOCHS_VAL_PERIOD,
+        model_name="TFT",
+        hidden_size = int(model_args['out_len']+((2/3)*model_args['in_len'])),
+        lstm_layers= model_args['lstm_layers'],
+        num_attention_heads = model_args['num_attention_heads'],
+        loss_fn=SmapeLoss(),
+        dropout=model_args['dropout'],
+        likelihood=QuantileRegression(quantiles=QUANTILES),
+        optimizer_kwargs={'lr': model_args['lr']},
+        add_encoders=encoders,
+        log_tensorboard=False,
+        force_reset=True,
+        save_checkpoints=save_checkpoints,
+        pl_trainer_kwargs=pl_trainer_kwargs,
+        torch_metrics=torch_metrics,
+        )
+    val_len = len(ts_test)
+    val_series = ts_ttrain[-((val_len) + model_args['in_len']) :]
+    ts_ttrain_input = ts_ttrain[:-(val_len )]
+    model.fit(  ts_ttrain, 
+                #future_covariates=covF_t,
+                val_series=ts_t[-((val_len)+model_args['in_len']) :],
+                #val_future_covariates=covF_t,
+             )
+    if save_model:
+        print("have saved the model after training:", mpath)
+        model.save_model(mpath)
+import torch
+from darts.utils.losses import SmapeLoss
+from torchmetrics import MetricCollection, SymmetricMeanAbsolutePercentageError, MeanAbsolutePercentageError
+def build_fit_tft_model_return(
+    model_args,
+    save_checkpoints=False,
+    callbacks=None,
+    save_model=False
+):
+    BATCH_SIZE=64
+    MAX_EPOCHS=500
+    NR_EPOCHS_VAL_PERIOD=1
+
+    torch_metrics = MetricCollection([MeanAbsolutePercentageError(), SymmetricMeanAbsolutePercentageError()])
+
+#     early_stopper = EarlyStopping(
+#         monitor="val_loss",
+#         patience=5,
+#         min_delta=0.001,
+#         mode='min',
+#     )
+
+#     if callbacks is None:
+#         callbacks = [early_stopper]
+#     else:
+#         callbacks.append(early_stopper)
+    
+    #detect if GPU is available
+#     if torch.cuda.is_available():
+#         pl_trainer_kwargs = {
+#             "accelerator": "gpu",
+#             "gpus": -1,
+#             "auto_select_gpus": True,
+#             "callbacks": callbacks,
+#             "enable_progress_bar":False,
+#         }
+#         num_workers=8
+#     else:
+#         pl_trainer_kwargs={
+#             "callbacks": callbacks,
+#         }
+#         num_workers=0
+    pl_trainer_kwargs={
+            "accelerator": "gpu",
+            "gpus":-1,
+            "auto_select_gpus": True,
+            "callbacks": callbacks,
+            "enable_progress_bar": False,
+        }
+    encoders={"cyclic": {"future": ["hour"]}} if model_args['include_hour'] else None
+
+
+    model = TFTModel(
+        input_chunk_length=model_args['in_len'],
+        output_chunk_length=model_args['out_len'],
+        batch_size=BATCH_SIZE,
+        n_epochs=MAX_EPOCHS,
+        nr_epochs_val_period=NR_EPOCHS_VAL_PERIOD,
+        model_name="TFT",
+        hidden_size = int(model_args['out_len']+((2/3)*model_args['in_len'])),
+        lstm_layers= model_args['lstm_layers'],
+        num_attention_heads = model_args['num_attention_heads'],
+        loss_fn=SmapeLoss(),
+        dropout=model_args['dropout'],
+        likelihood=QuantileRegression(quantiles=QUANTILES),
+        optimizer_kwargs={'lr': model_args['lr']},
+        add_encoders=encoders,
+        log_tensorboard=False,
+        force_reset=True,
+        save_checkpoints=save_checkpoints,
+        pl_trainer_kwargs=pl_trainer_kwargs,
+        torch_metrics=torch_metrics,
+        )
+    val_len = len(ts_test)
+    # val_series = ts_ttrain[-((val_len) + model_args['in_len']) :]
+    # ts_ttrain_input = ts_ttrain[:-(val_len )]
+    model.fit(  ts_ttrain, 
+                #future_covariates=covF_t,
+                val_series=ts_t[-((val_len)+model_args['in_len']) :],
+                #val_future_covariates=covF_t,
+             )
+    if save_model:
+        print("have saved the model after training:", mpath)
+        model.save_model(mpath)
+    return model
+
+from ray import tune
+from ray.tune import CLIReporter
+from ray.tune.integration.pytorch_lightning import TuneReportCallback
+from ray.tune.schedulers import ASHAScheduler, AsyncHyperBandScheduler
+from ray.tune.search.optuna import OptunaSearch
+tune_callback = TuneReportCallback(
+    {
+        "loss":"val_loss",
+        "sMAPE": "val_SymmetricMeanAbsolutePercentageError",
+    },
+    on="validation_end",
+)
+
+early_stopper = EarlyStopping(
+        monitor="val_SymmetricMeanAbsolutePercentageError",
+        patience=20,
+        min_delta=0.001,
+        mode='min',
+    )
+
+#define the hyperparameter search space
+config = {
+    "in_len": tune.choice([4,8,16,32,64, 128]),
+    "out_len":tune.randint(1,12),
+    "lstm_layers":tune.randint(1,4),
+    "num_attention_heads":tune.randint(1,12),
+    "dropout":tune.uniform(0.1,0.5),
+    "lr":tune.loguniform(1e-5,1e-1),
+    "include_hour":tune.choice([True]),
+}
+
+reporter = CLIReporter(
+    parameter_columns=list(config.keys()),
+    metric_columns=["loss", "sMAPE", "training_iteration"])
+resources_per_trial = {"cpu": 8, "gpu": 1}
+
+num_samples = 300
+
+algo = OptunaSearch()
+
+scheduler = AsyncHyperBandScheduler(max_t=100, grace_period=10, reduction_factor=3)
+
+train_fn_with_parameters = tune.with_parameters(build_fit_tft_model, callbacks=[early_stopper,tune_callback])
+
+analysis = tune.run(
+    train_fn_with_parameters,
+    resources_per_trial=resources_per_trial,
+    metric="sMAPE",
+    mode="min",
+    config=config,
+    num_samples=num_samples,
+    search_alg=algo,
+    scheduler = scheduler,
+    progress_reporter=reporter,
+    name="tft_tune_no_cov",
+    local_dir="./ray_results",
+
+)
+
+with open('best_hyper_no_cov.txt', 'w+') as f:
+    f.write(f"{analysis.best_config}")
